3d445d3ce29fda1e6fa79b836ebe43487d99ab83
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-13 17:19:18 -0400

    Implemented a breed fetcher program that fetched breed description from a breed api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,21 @@
-const needle = require ('needle');
+const needle = require('needle');
 
+
+const input = process.argv.splice(2);
+
+
+
+needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
+  //Handle invalid search request
+  
+
+  if (error) {
+    console.log('There seem to be a problem with the URL, Please check the URL or try a valid one.');
+  } else if (!body[0]) {
+    console.log('Breed not found.');
+    return;
+  } else {
+    console.log(body[0].name, ':');
+    console.log(body[0].description);
+  }
+});
