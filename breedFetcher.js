22a6f5c56f678307599efe7ee4d0918449b13aaf
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-07-13 17:51:14 -0400

    implemented a user facing index.js program and refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,13 @@
 const needle = require('needle');
 
+const fetchBreedDescription = function(breedName, callback) {
 
-const input = process.argv.splice(2);
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    callback(error, body[0].description);
+  });
 
+};
 
-
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  //Handle invalid search request
-  
-
-  if (error) {
-    console.log('There seem to be a problem with the URL, Please check the URL or try a valid one.');
-  } else if (!body[0]) {
-    console.log('Breed not found.');
-    return;
-  } else {
-    console.log(body[0].name, ':');
-    console.log(body[0].description);
-  }
-});
+module.exports = {
+  fetchBreedDescription,
+};
